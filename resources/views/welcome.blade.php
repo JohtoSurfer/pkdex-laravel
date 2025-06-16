572fba36a0dba9105910b823d4a18a00b1e1f4f9
Luis Barrios <104099952+JohtoSurfer@users.noreply.github.com>
2025-06-16 11:34:34 -0400

    pokemon card boostrap added with pikachu as example.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -273,11 +273,28 @@
         @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
         @endif
+        <!-- Ejemplo de tarjeta pokemon boostrap -->
+         <div class="container my-5">
 
-         <div class="container ">
-        <h1 class="text-primary">Rama para trabajar en el Login</h1>
-        <!--<button class="btn btn-success">Botón de prueba</button>!-->
-        <button type="button" class="btn btn-outline-warning">Prueba</button>
+    <h1 class="text-center mb-4">Pokédex</h1>
+
+    <div class="card mx-auto" style="width: 22rem;">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" class="card-img-top" alt="Pikachu">
+        <div class="card-body">
+            <h5 class="card-title">#025 - Pikachu</h5>
+            <p><strong>Tipos:</strong> Eléctrico</p>
+            <p><strong>Habilidades:</strong> Static, Lightning Rod</p>
+            <p><strong>Altura:</strong> 0.4 m</p>
+            <p><strong>Peso:</strong> 6 kg</p>
+            <p><strong>Descripción:</strong> Cuando se enfada, descarga electricidad de sus mejillas.</p>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+  <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+</svg>
+        </div>
     </div>
+
+
+</div>
     </body>
 </html>
